b1c42727f287f082f8c44850ada8cfb66fe34b86
med.wf <med.wf95@gmail.com>
2024-02-01 22:57:02 +0100

    start with States GET and DELETE

---
+++ api/v1/views/states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""import module"""
+from api.v1.views import app_views
+from flask import jsonify, make_response
+from models import storage
+from models.state import State
+
+
+@app_views.route("/states/<state_id>", strict_slashes=False, methods=["GET"])
+@app_views.route("/states", strict_slashes=False, methods=["GET"])
+def states(state_id=None):
+    """return a JSON: list dict or one or not found"""
+    if state_id == None:
+        result = []
+        states = storage.all(State).values()
+        for state in states:
+            result.append(state.to_dict())
+        return jsonify(result)
+    else:
+        state = storage.get(State, state_id)
+        if state == None:
+            return make_response(jsonify({"error": "Not found"}), 404)
+        return jsonify(state.to_dict())
+
+
+@app_views.route("/states/<state_id>", strict_slashes=False, methods=["DELETE"])
+def delete_states(state_id):
+    """return a JSON: delete object state with id"""
+    state = storage.get(State, state_id)
+    if state == None:
+        return make_response(jsonify({"error": "Not found"}), 404)
+    storage.all().pop(f"State.{state.id}")
+    storage.save()
+    return make_response(jsonify({}), 200)
