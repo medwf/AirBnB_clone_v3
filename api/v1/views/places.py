cb255328d5b5633529f665eabd29121f3c1d1cc6
aboubakr <c.aboubakr@gmail.com>
2024-02-03 13:26:31 +0100

    update the file api/v1/views/places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""view for Place objects that handles all default RESTFul API actions"""
+"""import module"""
 from flask import jsonify, make_response, request
 from api.v1.views import app_views
 from models import storage
