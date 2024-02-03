98a9e2a915b486e22b9cdfb276718e5187c37038
med.wf <med.wf95@gmail.com>
2024-02-03 15:47:38 +0100

    require

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""Flask API for AirBnB
-
-This script sets up a Flask application for an AirBnB API, providing endpoints
-to interact with AirBnB data"""
-import os
+"""This script sets up a Flask application for an AirBnB API
+providing endpoints to interact with AirBnB data"""
 from flask import Flask, jsonify
 from flask_cors import CORS
 from api.v1.views import app_views
 from models import storage
-
+import os
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
@@ -31,14 +28,13 @@ def not_found(error):
     return error.description, 400
 
 
-if os.getenv("HBNB_API_HOST"):
-    HOST = os.getenv("HBNB_API_HOST")
-else:
-    HOST = "0.0.0.0"
-if os.getenv("HBNB_API_PORT"):
-    PORT = int(os.getenv("HBNB_API_PORT"))
-else:
-    PORT = 5000
-
 if __name__ == '__main__':
+    if os.getenv("HBNB_API_HOST"):
+        HOST = os.getenv("HBNB_API_HOST")
+    else:
+        HOST = "0.0.0.0"
+    if os.getenv("HBNB_API_PORT"):
+        PORT = int(os.getenv("HBNB_API_PORT"))
+    else:
+        PORT = 5000
     app.run(host=HOST, port=PORT, threaded=True)
