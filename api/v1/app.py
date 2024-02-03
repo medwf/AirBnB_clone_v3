bd280edc447f80e9b014e15c39672824c27f7f6d
med.wf <med.wf95@gmail.com>
2024-02-03 12:19:37 +0100

    add  HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,12 @@ from models import storage
 from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
 from os import getenv
+from flask_cors import CORS
 
 """creating a Flask app"""
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views, url_prefix="/api/v1")
 """creating a Flask app"""
 
