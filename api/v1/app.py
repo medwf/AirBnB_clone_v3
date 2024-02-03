9f9b57818648b33b1bd97daafc7b942fb8977b99
med.wf <med.wf95@gmail.com>
2024-02-03 21:36:49 +0100

    change url

--- api/v1/app.py
+++ api/v1/app.py
@@ -29,7 +29,6 @@ def not_found(_):
 
 
 if __name__ == "__main__":
-    # Check for environment variables to configure host and port
-    HOST = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    PORT = int(os.getenv("HBNB_API_PORT", 5000))
+    HOST = os.getenv('HBNB_API_HOST', "0.0.0.0")
+    PORT = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=HOST, port=PORT, threaded=True)
