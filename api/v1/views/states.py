f2449c90f70f0e57a641531c25b29ee8abb1e435
med.wf <med.wf95@gmail.com>
2024-02-04 16:55:02 +0100

    state update create

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -25,37 +25,31 @@ def states(state_id=None):
         return jsonify(state.to_dict())
 
 
-@app_views.route("/states/<state_id>", strict_slashes=False,
+@app_views.route("/states/<state_id>",
+                 strict_slashes=False,
                  methods=["DELETE"])
-def states_delete(state_id):
-    """delete method"""
-    obj = storage.get(State, state_id)
-    if obj is None:
+def delete_states(state_id):
+    """return a JSON: delete a state object that match State_id
+    or Not found if id not exist"""
+    state = storage.get(State, state_id)
+    if state is None:
         abort(404)
-    storage.delete(obj)
+    storage.delete(state)
     storage.save()
     return jsonify({}), 200
 
 
 @app_views.route("/states", strict_slashes=False, methods=["POST"])
-def Create_state():
-    """
-    If the HTTP body request is not valid JSON,
-        raise a 400 error with the message Not a JSON
-    If the dictionary doesn't contain the key name,
-        raise a 400 error with the message Missing name
-    Returns: the new State with the status code 201
-    """
-    json_data = request.get_json(force=True, silent=True)
-    if json_data:
-        if "name" in json_data:
-            instance = State(**json_data)
-            instance.save()
-            return jsonify(instance.to_dict), 201
-        else:
-            abort(400, "Missing name")
-    else:
+def create_state():
+    """create a new post req"""
+    data = request.get_json(force=True, silent=True)
+    if not data:
         abort(400, "Not a JSON")
+    if "name" not in data:
+        abort(400, "Missing name")
+    new_state = State(**data)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
 
 
 @app_views.route("/states/<state_id>", strict_slashes=False, methods=["PUT"])
